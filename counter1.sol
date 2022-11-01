adf92473aff91a92548f3ac5cbcc3c77041f2e46
Remix Workspace <>
2022-11-01 13:30:19 -0500

    counter

---
+++ counter1.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.5.1;
+
+contract Counter{
+    uint count = 0 ;
+     event Increment(uint value);
+     event Decrement(uint value);
+   
+    constructor() public{
+        count=0;
+    }
+
+    function getCount() view public returns(uint){
+        return count;
+    }
+     function increment() public{
+         count +=1;
+        emit Increment(count);
+     }
+
+     function decrement() public{
+         count-=1;
+         emit Decrement(count);
+     }
+}
